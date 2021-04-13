6db77f728833f0c40b76146c393a8816ba8e110e
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-13 14:44:47 -0500

    Set up redux @ index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// Libraries
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+// Split(?) and import reducers
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+///////////////////// REDUCER /////////////////////
+
+const initialState = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: "2019 Ford Mustang",
+    image:
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+    features: [],
+  },
+  additionalFeatures: [
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 },
+    { id: 5, name: "Sparkles in the paint", price: 3500 },
+  ],
+};
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+///////////////////// STORE /////////////////////
+
+const store = createStore(reducer);
+console.log(store.getState());
+
+/*
+imports
+create store
+  create reducer
+  pass reducer to createStore
+wrap App in Provider
+pass store as Provider prop
+*/
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
